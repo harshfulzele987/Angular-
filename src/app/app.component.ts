123a8313df91f595255d39543bee66f3ddb3242b
harsh fulzel <harshfulzele987@gmail.com>
2021-02-27 17:02:50 +0530

    angular files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+ import { Component } from '@angular/core';
 
+
+function log(target, name ,discripter){
+  console.log(target,name,discripter)
+}
 @Component({
+
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
+
+
 export class AppComponent {
+  
   title = 'into2angular';
+
+
+    @log
+    asimpleMethod()
+    {
+      console.log("hey there")
+    }
 }
